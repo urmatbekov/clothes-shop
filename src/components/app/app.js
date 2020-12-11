8032c8b9e75ebc0ce2d2df9ab9e7217d13e6d0a3
Ваше Имя <you@example.com>
2020-12-11 16:55:14 +0600

    register

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,11 +4,14 @@ import UseScript from "../hooks/useScript";
 import Cart from "../cart/cart";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
 import Home from "../pages/home/home";
+import Service from "../service";
 import Register from "../pages/register/register";
 
 
 class App extends Component {
+    componentDidMount() {
 
+    }
 
     render() {
 
@@ -23,6 +26,7 @@ class App extends Component {
                     <Route exact path={"/register"}>
                         <Register/>
                     </Route>
+
                 </Switch>
                 <UseScript url={"js/active.js"}/>
             </Router>
